8cdafb74eb4e797338e0aa5ca06c8b05994c6695
obihakenobi <joshua.obiha@hotmail.com>
2019-05-04 00:37:19 -0400

    May 4th - modified casualhome.html and main.js

--- web/script/main.js
+++ web/script/main.js
@@ -1,9 +1,13 @@
+const pro    = '#7492cc'; //Color for Professional site background
+const casual = '#23272A'; //Color for Casual site background
 const signUpButton        = document.getElementById('signUp');
 const signInButton        = document.getElementById('signIn');
 const container           = document.getElementById('container');
-const pro    = '#7492cc';
-const casual = '#23272A';
+var   menu                = document.querySelector('ul');
+var   menuLink            = document.querySelector('img');
+const resumeButton        = document.querySelector('button-one');
 
+//SignIn.html/////////////////////////////////////////////////////////////////////////////////////////////////////////
 signUpButton.addEventListener('click', () => {
     container.classList.add("right-panel-active");
     document.body.style.background = pro;    
@@ -13,3 +17,15 @@ signInButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
     document.body.style.background = casual;
 });
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+resumeButton.addEventListener('click', () =>{
+    console.log("This is working")
+    // window.open("web/pdf/Resume.pdf");
+})
+
+menuLink.addEventListener('click',(e) => {
+    menu.classList.toggle('active');
+    e.preventDefault();
+})
